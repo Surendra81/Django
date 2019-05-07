4236cc5851f800cea6d3679afe65ee97270e03bb
Surendra81 <ariesgaire@gmail.com>
2019-05-07 23:38:04 +0545

    shortcuts key was used using render

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from . models import Question
 from django.template import loader
+from django.shortcuts import render
 
 
 def index(request):
@@ -8,11 +9,10 @@ def index(request):
 
 def index(request):
     latest_question_list=Question.objects.order_by('-pub_date')[:5]
-    template=loader.get_template('polls/index.html')
     context={
         'latest_question_list':latest_question_list,
-}
-    return HttpResponse(template.render(context,request))
+        }
+    return (request, 'polls/index.html', context)
 
 def detail(request,question_id):
     return HttpResponse("you're looking at question. %s" %question_id )
