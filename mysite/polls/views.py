3ccddb6524749cc3df86754ca927b7f3b5d1d8d0
Surendra81 <ariesgaire@gmail.com>
2019-06-09 14:20:40 +0545

    update in views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -22,4 +22,16 @@ def results(request,question_id):
     return HttpResponse(response % question_id)
 
 def vote(request,question_id):
-    return HttpResponse("you're voting on question %s " %question_id)
+    question=get_object_or_404(Question,pk=question_id)
+    try:
+        selected_choice=question.choice_set.get(pk=request.POST['choice'])
+    except(keyError,Choice.DoesNotExist):
+        #redisplay question voting form
+        render(request,'polls/detail.html',{'question':question, 'error_message':'you didnt select a choice.',})
+    else:
+        selected_choice.votes +=1
+        selected.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results',args=(question.id)))
